0636f717b630e269259a770935169b0b99401a72
SyahfrudinAji <syahfrudinaji>
2018-07-19 22:47:55 +0700

    tables

---
+++ resources/views/books-ajax/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')	
+<div class="col-md-8 col-md-offset-2">
+	<h3>Data Buku</h3>	
+	<table class="table" id="books">
+		<thead>
+			<tr>
+				<th>Id</th>
+				<th>Nama</th>
+				<th>Genre</th>
+				<th>Harga</th>
+			</tr>
+		</thead>
+	</table>
+</div>
+@endsection
+
+@section('scripts')	
+<script type="text/javascript">
+	$(function(){
+		$("#books").DataTable({
+			processing: true,
+	        serverSide: true,
+	        ajax: '{!! route('book.data') !!}',
+	        columns: [
+	            { data: 'id', name: 'id' },
+	            { data: 'nama', name: 'nama' },
+	            { data: 'genre', name: 'genre' },
+	            { data: 'harga', name: 'harga' }
+	        ]
+		});
+	});	
+</script>
+@endsection
+
